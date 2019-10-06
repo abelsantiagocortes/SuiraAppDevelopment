1e2ef5c43a2225bd481542979433d47c9d06f5d3
David <sarmientodav13@gmail.com>
2019-10-05 23:19:45 -0500

    First screen: Log In Layout

---
+++ app/src/main/java/com/development/SuiraApp/Forgot_Password_Layout.java
@@ -0,0 +1,14 @@
+package com.development.SuiraApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forgot_Password_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot__password__layout);
+    }
+}
