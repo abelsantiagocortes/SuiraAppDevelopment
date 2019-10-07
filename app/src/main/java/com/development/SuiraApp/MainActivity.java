407735e990883e73c831bd92532f782f3fe0b475
camiloserr <camiloserrano@javeriana.edu.co>
2019-10-07 14:34:45 -0500

    Register activity front end is ready

--- app/src/main/java/com/development/SuiraApp/MainActivity.java
+++ app/src/main/java/com/development/SuiraApp/MainActivity.java
@@ -2,7 +2,11 @@ package com.development.SuiraApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button boton = findViewById(R.id.registrarse);
+        boton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this , RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
