146b05d2a0a5281fc613365c9cfbdae74a3e9822
Abel Cortes <abelsantiagocortes@gmail.com>
2019-10-11 15:36:08 -0500

    Create Opportunity Second Tab ( No Color )

---
+++ app/src/main/java/com/development/SuiraApp/CreateOpportunity_2.java
@@ -0,0 +1,14 @@
+package com.development.SuiraApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateOpportunity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_opportunity_2);
+    }
+}
