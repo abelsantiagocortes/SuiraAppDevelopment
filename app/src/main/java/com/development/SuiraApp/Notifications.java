4c4f48bc2c1e58708093b97b917da0ee5304e2d3
abelsantiagocortes <abelsantiagocortes@gmail.com>
2019-10-12 21:35:37 -0500

    Notifications with Divider RecycleView

--- app/src/main/java/com/development/SuiraApp/Notifications.java
+++ app/src/main/java/com/development/SuiraApp/Notifications.java
@@ -1,6 +1,8 @@
 package com.development.SuiraApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,6 +10,8 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.security.AccessController.getContext;
 
 public class Notifications extends AppCompatActivity {
 
@@ -23,9 +27,15 @@ public class Notifications extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.rv);
 
 
+
         LinearLayoutManager llm = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(llm);
         recyclerView.setHasFixedSize(true);
+
+        DividerItemDecoration itemDecorator = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        itemDecorator.setDrawable(ContextCompat.getDrawable(this, R.drawable.divider));
+        recyclerView.addItemDecoration(itemDecorator);
+
 
         initializeData();
         initializeAdapter();
