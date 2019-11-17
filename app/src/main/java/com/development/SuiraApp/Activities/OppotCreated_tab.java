d0ed586b2495bcd427a1d14c0a9bc71f1b5036c4
Juan Sebastian Osorio Garcia <seposorio@hotmail.com>
2019-11-17 02:38:25 -0500

    All tabs working without style in Oppor

---
+++ app/src/main/java/com/development/SuiraApp/Activities/OppotCreated_tab.java
@@ -0,0 +1,22 @@
+package com.development.SuiraApp.Activities;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.development.SuiraApp.MyViewPagerAdapter;
+import com.development.SuiraApp.R;
+
+public class OppotCreated_tab extends Fragment {
+
+    private MyViewPagerAdapter adapter;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_opporcreated, container, false);
+        return  view;
+    }
+}
